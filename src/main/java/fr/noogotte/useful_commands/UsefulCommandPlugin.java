9e07f6914ecc15dda9ac3ff2baadb80858fea806
noogotte <hugotte.76@live.fr>
2012-06-06 15:46:55 +0200

    Add config

--- src/main/java/fr/noogotte/useful_commands/UsefulCommandPlugin.java
+++ src/main/java/fr/noogotte/useful_commands/UsefulCommandPlugin.java
@@ -1,9 +1,19 @@
 package fr.noogotte.useful_commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import fr.aumgn.bukkitutils.command.CommandsRegistration;
 import fr.aumgn.bukkitutils.command.messages.FrenchMessages;
+import fr.aumgn.bukkitutils.gconf.GConfLoadException;
+import fr.aumgn.bukkitutils.gconf.GConfLoader;
+import fr.aumgn.bukkitutils.gconf.typeadapter.DirectionTypeAdapterFactory;
 import fr.noogotte.useful_commands.command.AfkCommand;
 import fr.noogotte.useful_commands.command.GodCommand;
 import fr.noogotte.useful_commands.command.PlayerCommands;
@@ -16,18 +26,51 @@ public class UsefulCommandPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        GodComponent godComponent = new GodComponent(this);
-        WarpsComponent warpComponent = new WarpsComponent();
-        AfkComponent afkcComponent = new AfkComponent(this);
+        UsefulConfig config;
+        GConfLoader loader = getConfigLoader();
+        try {
+            config = loader.loadOrCreate("config.json", UsefulConfig.class);
+        } catch (GConfLoadException exc) {
+            getLogger().severe("Unable to load config.json. Using default values");
+            config = new UsefulConfig();
+        }
 
         CommandsRegistration registration = new CommandsRegistration(
                 this, new FrenchMessages());
+
+        GodComponent godComponent = new GodComponent(this);
+        AfkComponent afkComponent = new AfkComponent(this);
         registration.register(new WorldCommands());
         registration.register(new PlayerCommands());
-        registration.register(new GodCommand(godComponent));
         registration.register(new TeleportCommands());
-        registration.register(new WarpsCommand(warpComponent));
-        registration.register(new AfkCommand(afkcComponent));
-        registration.register(new PlayerInfoCommand(godComponent, afkcComponent));
+        registration.register(new AfkCommand(afkComponent));
+        registration.register(new PlayerInfoCommand(godComponent, afkComponent));
+        
+        if (config.enableGod()) {
+            
+            registration.register(new GodCommand(godComponent));
+        }
+
+        if (config.enableWarp()) {
+            WarpsComponent warpcomponent = new WarpsComponent();
+            registration.register(new WarpsCommand(warpcomponent));
+        }
+    }
+
+    private GConfLoader getConfigLoader() {
+        Gson gson = new GsonBuilder()
+            .registerTypeAdapterFactory(new DirectionTypeAdapterFactory())
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)
+            .setPrettyPrinting()
+            .create();
+        return new GConfLoader(gson, this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd,
+            String lbl, String[] args) {
+        sender.sendMessage(ChatColor.RED +
+                "Cette commande n'est pas activée.");
+        return true;
     }
 }
---
+++ src/main/java/fr/noogotte/useful_commands/UsefulConfig.java
@@ -0,0 +1,15 @@
+package fr.noogotte.useful_commands;
+
+public class UsefulConfig {
+
+    private boolean enableGod = true;
+    private boolean enableWarp = true;
+
+    public boolean enableGod() {
+        return enableGod;
+    }
+
+    public boolean enableWarp() {
+        return enableWarp;
+    }
+}
