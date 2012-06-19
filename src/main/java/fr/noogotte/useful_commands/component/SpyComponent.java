e0439ca710176ec72b18c720de2734df3c6f1a59
noogotte <hugotte.76@live.fr>
2012-06-19 21:55:23 +0200

    Clean up

--- src/main/java/fr/noogotte/useful_commands/component/SpyComponent.java
+++ src/main/java/fr/noogotte/useful_commands/component/SpyComponent.java
@@ -9,7 +9,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
