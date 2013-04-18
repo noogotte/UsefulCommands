a6e17a299ce2bd9d0e5d338a2af25d65969efcd9
noogotte <hugotte.76@live.fr>
2013-04-18 16:45:34 +0200

    Add a way to set the spawned mob from a spawner with an mob's egg

--- src/main/java/fr/noogotte/useful_commands/component/PlayerComponent.java
+++ src/main/java/fr/noogotte/useful_commands/component/PlayerComponent.java
@@ -1,15 +1,27 @@
 package fr.noogotte.useful_commands.component;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
 import fr.aumgn.bukkitutils.command.Commands;
 import fr.noogotte.useful_commands.UsefulCommandsPlugin;
 import fr.noogotte.useful_commands.command.InfoServerCommands;
 import fr.noogotte.useful_commands.command.PlayerCommands;
 import fr.noogotte.useful_commands.command.PlayerInfoCommand;
 
-public class PlayerComponent extends Component {
+public class PlayerComponent extends Component implements Listener {
 
     public PlayerComponent(UsefulCommandsPlugin plugin) {
         super(plugin);
+        Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
@@ -24,5 +36,40 @@ public class PlayerComponent extends Component {
                 new PlayerInfoCommand(plugin),
                 new InfoServerCommands(plugin)
         };
+    }
+    
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+
+        Block block = event.getClickedBlock();
+        if (block.getType() != Material.MOB_SPAWNER) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        if (player.getItemInHand().getTypeId() != 383) {
+            return;
+        }
+
+        if (!player.hasPermission("useful.player.changemmobspawner")) {
+            return;
+        }
+
+        String data = player.getItemInHand().getData().toString();
+        String mobName = data.replace("SPAWN EGG", "").replace("{", "")
+                .replace("}", "").replace("_", "");
+        if (mobName.equalsIgnoreCase("MAGMACUBE")) {
+            mobName = "LavaSlime";
+        } else if (mobName.equalsIgnoreCase("OCELOT")) {
+            mobName = "Ozelot";
+        }
+        CreatureSpawner spawner = (CreatureSpawner) block.getState();
+        EntityType type = EntityType.fromName(mobName);
+        spawner.setSpawnedType(type);
+        player.sendMessage(plugin.getMessages().get("setSpawnerType",
+                type.getName()));
     }
 }
